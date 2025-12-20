6152f11e281c33c6a881d15a3d9684822bc98ff8
ahnafi <devsulthon@gmail.com>
2025-12-20 15:15:51 +0700

    fix : prefix /api

--- routes/api.php
+++ routes/api.php
@@ -4,4 +4,4 @@ use App\Http\Controllers\YouTubeController;
 use Illuminate\Support\Facades\Route;
 
 // YouTube Videos API
-Route::get('/api/youtube-videos', [YouTubeController::class, 'getLatestVideos']);
+Route::get('/youtube-videos', [YouTubeController::class, 'getLatestVideos']);
